e6d3355eab953730ca469df3153b32f63d0b450d
Mattias Svensson <190869+mattsve@users.noreply.github.com>
2026-05-08 07:55:21 +0200

    Use admesh

--- generate.sh
+++ generate.sh
@@ -5,6 +5,7 @@ seds+="s/ö/o/g;"
 seds+="s/Å/A/g;"
 seds+="s/Ä/A/g;"
 seds+="s/Ö/O/g;"
+seds+="s/ü/u/g;"
 
 [[ -d stl ]] || mkdir stl
 while IFS= read -r line ; do
@@ -12,6 +13,7 @@ while IFS= read -r line ; do
   stl="stl/${trimmed}.stl"
   if [ ! -f "$stl" ]; then
     txt=$(echo "$trimmed" | sed ${seds})
-    npx jscad template.js --text "${txt}" -o "$stl" 
+    npx jscad template.js --text "${txt}" -o "$stl"
+    admesh --fill-holes --normal-directions --normal-values -b "$stl" "$stl" > /dev/null
   fi
 done < names.txt
